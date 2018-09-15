69c0c4b4d4844b7547f5080f21fa6288dddb02fc
Sajal Sharma <sajal2692@gmail.com>
2018-09-15 21:15:23 +0530

    feature: added ETL pipeline

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,69 @@
 import sys
+import pandas as pd
+from sqlalchemy import create_engine
 
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    """
+    Loads the messages and categories datasets from the specified filepaths
+
+    Args:
+        messages_filepath: Filepath to the messages dataset
+        categories_filepath: Filepath to the categories dataset
+
+    Returns:
+        (DataFrame) df: Merged Pandas dataframe
+
+    """
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    df = messages.merge(categories, on='id')
+    return df
 
 
 def clean_data(df):
-    pass
+    """
+    Cleans the merged dataset
+
+    Args:
+        df: Merged pandas dataframe
+
+    Returns:
+        (DataFrame) df: Cleaned dataframe
+    """
+    categories = df['categories'].str.split(';',expand=True)
+    row = categories[:1]
+    category_colnames = row.apply(lambda x: x.str.split('-')[0][0], axis=0)
+    categories.columns = category_colnames
+
+    # convert category values to just numbers 0 or 1
+    for column in categories:
+        categories[column] = categories[column].apply(lambda x: x.split('-')[1] if int(x.split('-')[1]) < 2 else 1)
+        categories[column] = categories[column].astype(int)
+
+    # drop the original categories column from `df`
+    df.drop('categories',axis=1,inplace=True)
+
+    # concatenate the original dataframe with the new `categories` dataframe
+    df = pd.concat([df,categories], axis=1)
+
+    # drop duplicates
+    df = df.drop_duplicates(keep='first')
+
+    return df
 
 
 def save_data(df, database_filename):
-    pass  
+    """
+    Saves clean dataset into an sqlite database
+
+    Args:
+        df:  Cleaned dataframe
+        database_filename: Name of the database file
+    """
+
+    engine = create_engine('sqlite:///etlProject.db')
+    df.to_sql(database_filename, engine, index=False)
 
 
 def main():
@@ -24,12 +77,12 @@ def main():
 
         print('Cleaning data...')
         df = clean_data(df)
-        
+
         print('Saving data...\n    DATABASE: {}'.format(database_filepath))
         save_data(df, database_filepath)
-        
+
         print('Cleaned data saved to database!')
-    
+
     else:
         print('Please provide the filepaths of the messages and categories '\
               'datasets as the first and second argument respectively, as '\
